1483c76bc41e3241d95f3849735b55f8c63c1d47
Michał Szeniak <michal.szeniak@gmail.com>
2020-05-26 23:49:40 +0200

    feat: attached api

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const api = { 
     key: '2faf5362af8831e653fa8cc1ca8dd326',
-    baseurl: 'api.openweathermap.org/data/2.5/weather?q='
+    baseurl: 'http://api.openweathermap.org/data/2.5/weather?q='
 }
 
 const searchbox = document.querySelector(".searchbox");
+const city = document.querySelector(".city")
+
 searchbox.addEventListener('keypress', (evt) => {
     if (evt.keyCode == 13) {
-        searchValue = searchbox.value
+        searchValue = searchbox.value;
         console.log(searchValue);
         apiUrl ()
     }
 });
 
 const apiUrl = () => {
-    const url = `${api.baseurl}${searchValue}&units=metrinc&APPID=${api.key}`;
-    console.log(url);
+    const apiiurl = `${api.baseurl}${searchValue}&units=metric&APPID=${api.key}`;
+    console.log(apiiurl);
 
-    fetch(url)
-        .then((data) => console.log(data.json()))
+    fetch(apiiurl)
+        .then( (data) => data.json())
+        .then( (weather) => generate(weather))
+generate()
+}
+
+const generate = (data) => {
+    console.log(data);
+    console.log(data.name);
+    console.log(`${data.main.temp}`);
+    city.innerHTML = data.name
 }
