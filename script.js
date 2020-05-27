9fbb276547217b309b859bc4fcac8453a56c0734
Michał Szeniak <michal.szeniak@gmail.com>
2020-05-27 17:26:43 +0200

    feat: style

--- script.js
+++ script.js
@@ -4,12 +4,26 @@ const api = {
 }
 
 const searchbox = document.querySelector(".searchbox");
-const city = document.querySelector(".city")
+const city = document.querySelector(".city");
+const icon = document.querySelector(".icon__src");
+const country = document.querySelector(".country");
+const temp = document.querySelector(".temp");
+const desctiption = document.querySelector(".desctiption");
+const minMax = document.querySelector(".min-max");
+const searchButton = document.querySelector(".search-button")
+const weatherContainer = document.querySelector(".weather-container")
+
+searchButton.addEventListener('click', () => {
+    searchValue = searchbox.value;
+    icon.classList.add("icon--js");
+    apiUrl ()
+})
 
 searchbox.addEventListener('keypress', (evt) => {
     if (evt.keyCode == 13) {
         searchValue = searchbox.value;
         console.log(searchValue);
+        icon.classList.add("icon--js");
         apiUrl ()
     }
 });
@@ -21,12 +35,19 @@ const apiUrl = () => {
     fetch(apiiurl)
         .then( (data) => data.json())
         .then( (weather) => generate(weather))
-generate()
 }
 
 const generate = (data) => {
-    console.log(data);
-    console.log(data.name);
-    console.log(`${data.main.temp}`);
-    city.innerHTML = data.name
+    city.innerHTML = `${data.name}`;
+    country.innerHTML = `${data.sys.country}`
+    temp.innerHTML = `${data.main.temp}°c`;
+    icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    desctiption.innerHTML = `${data.weather[0].description}`;
+    minMax.innerHTML = `${(data.main.temp_min).toFixed()}°c / ${(data.main.temp_max).toFixed()}°c`
+
+    if(data.main.temp < 0) {
+        weatherContainer.classList.add("weather-container--js");
+    } else {
+        weatherContainer.classList.remove("weather-container--js");
+    }
 }
